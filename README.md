bf93039769bc47259e1b7bba2694add974e075e8
SirYazoo <30350331+SirYazoo@users.noreply.github.com>
2025-01-18 14:54:38 +0700

    Add getComputerChoice function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-rock-paper-scirssors
+This is a project from The Odin Project. The project is about Rock Paper Scissors game using all JavaScript Basics knowledge learnt from The Odin Project.
---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let rng = Math.floor(Math.random() * 3);
+    if (rng === 0) {
+        return "rock";
+    } else if (rng === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
