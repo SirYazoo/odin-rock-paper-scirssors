833f64de62731941edf232e58cf908c9e8238aec
SirYazoo <30350331+SirYazoo@users.noreply.github.com>
2025-01-18 15:15:11 +0700

    Add logic to play a single round

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rng = Math.floor(Math.random() * 3);
     if (rng === 0) {
@@ -14,4 +17,50 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore++;
+            console.log("You win! Rock beats Scissors");
+        } else if (computerChoice === "paper") {
+            computerScore++;
+            console.log("You lose! Paper beats Rock");
+        } else {
+            console.log("It is a tie");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore++;
+            console.log("You win! Paper beats Rock");
+        } else if (computerChoice === "scissors") {
+            computerScore++;
+            console.log("You lose! Scissors beats Paper");
+        } else {
+            console.log("It is a tie");
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore++;
+            console.log("You win! Scissors beats Paper");
+        } else if (computerChoice === "rock") {
+            computerScore++;
+            console.log("You lose! Rock beats Scissors");
+        } else {
+            console.log("It is a tie");
+        }
+    } else {
+        console.log("Invalid input!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore);
+console.log(computerScore);
